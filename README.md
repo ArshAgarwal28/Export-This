e871f7ae89d3273ca5d4b7f67bd29d614696916a
ArshAgarwal28 <61740528+ArshAgarwal28@users.noreply.github.com>
2020-08-31 15:32:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Export-This
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,11 @@
+
+
+function setup(){
+  createCanvas(1200, 800);
+
+}
+
+function draw() {
+  background("white");
+
+}
